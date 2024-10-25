6de9435dfe06be26b3968731a6974584352827dd
Simon-Otalvaro <Simonotalvaro6@gmail.com>
2024-10-25 11:25:17 -0500

    first five arrays

---
+++ app.js
@@ -0,0 +1,52 @@
+const products = [{
+id: 1,
+name: "Jordan Retro IV - Off-White",
+description: "Los Jordan Retro 4 Off-White combinan el diseño clásico de Jordan con el estilo único de Off-White. Con un tono beige claro, materiales translúcidos y detalles en gamuza, estos tenis ofrecen una estética moderna y elegante. Perfectos para coleccionistas y amantes de la moda urbana.",
+price: 290.000,
+stack: 40,
+color: "Beige",
+size: 38-39-40,
+img: 'img\Jordan-Off White.webp'
+},
+
+{id: 2,
+name: "Jordan Retro IV - Military Black",
+description: "Los Jordan Retro 4 Military Black presentan un diseño clásico en blanco, negro y gris, ideal para cualquier ocasión. Con materiales de alta calidad y detalles en cuero y gamuza, ofrecen un estilo versátil y moderno que combina perfectamente con looks casuales y urbanos.",
+price: 250.000,
+stack: 20,
+color: "Mixto",
+size: 37-38,
+img: ''
+},
+
+{id: 3,
+name: "Jordan 3",
+description: "Los Jordan Retro 3 ofrecen un diseño icónico con detalles en cuero premium y el clásico estampado de elefante en el talón y en la puntera. Disponibles en color blanco clásico, estos tenis destacan por su estilo atemporal y comodidad, ideales para los amantes del estilo retro y urbano.",
+price: 260.000,
+stack: 10,
+color: "Mixto",
+size: 37-38,
+img: 'img\Jordan 3.jpg'
+},
+    
+{id: 4,
+name: "Air Force One - Dior",
+description: "Los Air Force 1 Dior son una colaboración exclusiva que fusiona el diseño clásico de Nike con la elegancia de Dior. Presentan materiales de alta calidad, con detalles en cuero y el patrón característico de Dior en los paneles laterales. Con su estilo sofisticado y detalles de lujo, estos tenis son ideales para quienes buscan exclusividad y alta moda en un calzado deportivo.",
+price: 300.000,
+stack: 6,
+color: "Gris",
+size: 38-39-40,
+img: ''
+},
+    
+{id: 5,
+name: "Jordan 1 Clasicas",
+description: "Las Jordan 1 Clásicas ofrecen el diseño icónico que definió una era, combinando materiales de alta calidad y un estilo atemporal. Disponibles en su color clásico, estos tenis son una elección imprescindible para los amantes del streetwear y de las zapatillas clásicas.",
+price: 230.000,
+stack: 50,
+color: "Negro, Blanco, Rojo",
+size: 38-39-40,
+img: ''
+},           
+
+]
