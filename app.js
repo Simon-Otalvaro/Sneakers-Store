39eade19947f431e64e97921da1b53b07df8fc1a
Simon-Otalvaro <Simonotalvaro6@gmail.com>
2024-10-28 07:14:25 -0500

    first ten arrays

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const products = [{
-id: 1,
+const products = [
+
+{id: 1,
 name: "Jordan Retro IV - Off-White",
 description: "Los Jordan Retro 4 Off-White combinan el diseño clásico de Jordan con el estilo único de Off-White. Con un tono beige claro, materiales translúcidos y detalles en gamuza, estos tenis ofrecen una estética moderna y elegante. Perfectos para coleccionistas y amantes de la moda urbana.",
 price: 290.000,
@@ -47,6 +48,56 @@ stack: 50,
 color: "Negro, Blanco, Rojo",
 size: 38-39-40,
 img: ''
-},           
+},
+
+{id: 6,
+name: "New Balance 550",
+description: "Las New Balance 550 combinan un diseño retro de los años 80 con un estilo urbano moderno. Con detalles en cuero y una suela resistente, ofrecen comodidad y versatilidad, ideales para quienes buscan un look casual y atemporal.",
+price: 190.000,
+stack: 30,
+color: "Mixto",
+size: 38-40,
+img: ''
+},
+
+{id: 7,
+name: "Nike Air Max 97",
+description: "Los Nike Air Max 97 destacan por su diseño inspirado en las ondas del agua y sus líneas futuristas. Con la icónica unidad Air de amortiguación completa y una combinación de materiales reflectantes y de alta calidad, estos tenis ofrecen comodidad y estilo, perfectos para un look moderno y llamativo.",
+price: 150.000,
+stack: 20,
+color: "Blanco",
+size: 38-39-40,
+img: ''
+},
+
+{id: 8,
+name: "Adidas Campus 00s",
+description: "Los Adidas Campus 00s son una reinvención moderna del clásico modelo Campus, con un diseño robusto y detalles retro de los años 2000. Fabricados en gamuza premium, con las emblemáticas tres franjas y una suela gruesa, ofrecen un look casual y nostálgico, ideal para el día a día.",
+price: 190.000,
+stack: 35,
+color: "Negro",
+size: 38-39,
+img: ''
+},
+
+{id: 9,
+name: "Balenciaga Triple S",
+description: "Las Balenciaga Triple S son un ícono de la moda urbana, caracterizadas por su diseño robusto de suela triple y una estética oversize. Confeccionadas en una mezcla de materiales de alta calidad, como cuero, malla y goma, estas zapatillas ofrecen un estilo vanguardista y atrevido, perfecto para quienes buscan un look de lujo y modernidad.",
+price: 210.000,
+stack: 5,
+color: "Beige",
+size: 40,
+img: ''
+},
+
+{id: 10,
+    name: "Yeezy 700",
+    description: "Las Yeezy 700 combinan un diseño retro con un toque futurista, destacando por su silueta voluminosa y suela gruesa. Fabricadas con materiales de alta calidad como malla, cuero y gamuza, ofrecen comodidad y estilo en cada paso. Perfectas para quienes buscan un look audaz y contemporáneo.",
+    stack: 10,
+    color: "",
+    size: 40,
+    img: 'Blanco'
+    },
+    
 
 ]
